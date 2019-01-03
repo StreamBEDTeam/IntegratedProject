5b4e939128d78bc4fff50fa09129df4c8ff20d92
Alina <alina.striner@gmail.com>
2019-01-03 18:36:09 -0500

    VR upload

---
+++ VR/Assets/Scripts/CameraController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Camera mainCamera;
+
+    private GameObject raycastFocus;
+
+    public Text DebugText;
+
+    private TourController tourController;
+
+    // Use this for initialization
+    void Start()
+    {
+        tourController = GameObject.Find("TourManager").GetComponent<TourController>();
+    }
+
+    // called every frame
+    void Update()
+    {
+        castRay();
+        checkControllerInput();
+    }
+
+    private void castRay()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast (mainCamera.transform.position, mainCamera.transform.TransformDirection(Vector3.forward), out hit)) {
+            raycastFocus = hit.collider.gameObject;
+            raycastFocus.SendMessage("onRaycastReceived");
+        }           
+        else
+        {
+            if (raycastFocus != null)
+            {
+                raycastFocus.SendMessage("onRaycastRemoved");
+                raycastFocus = null;
+            }
+        }
+    }
+
+    private void checkControllerInput()
+    {
+        bool isAPressed = Input.GetButton("A");
+        bool isBPressed = Input.GetButton("B");
+        bool isXPressed = Input.GetButton("X");
+        bool isYPressed = Input.GetButton("Y");
+
+        DebugText.text = string.Format("A: {0} B: {1} X: {2} Y: {3}\n", isAPressed, isBPressed, isXPressed, isYPressed);
+
+        if (isAPressed)
+        {
+            onAPressed();
+        }
+
+        if (isBPressed)
+        {
+            onBPressed();
+        }
+
+        if (isXPressed)
+        {
+            onXPressed();
+        }
+
+        if (isYPressed)
+        {
+            onYPressed();
+        }
+    }
+
+    private void onAPressed()
+    {
+        tourController.setCameraMode(true);
+    }
+
+    private void onBPressed()
+    {
+        tourController.setCameraMode(false);
+    }
+
+    private void onXPressed()
+    {
+
+    }
+
+    private void onYPressed()
+    {
+
+    }
+
+}
---
+++ VR/Assets/Scripts/MenuController.cs
@@ -0,0 +1,55 @@
+﻿using StreamBED.Backend.Helper;
+using StreamBED.Backend.Models.ProtocolModels;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public GameObject discardButton;
+
+    public GameObject saveButton;
+
+	// Use this for initialization
+	void Start () {
+        displayFeatureList();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnEnable()
+    {
+        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(discardButton);
+    }
+
+    private void displayFeatureList()
+    {
+        Keyword[] bKey = BankStabilityModel.GetKeywords();
+        Keyword[] eKey = EpifaunalSubstrateModel.GetKeywords();
+        Keyword[] featureNames = new Keyword[bKey.Length + eKey.Length];
+        bKey.CopyTo(featureNames, 0);
+        eKey.CopyTo(featureNames, bKey.Length);
+
+        // Add the features
+        int len = featureNames.Length; 
+        GameObject[] featureObjs = new GameObject[len];
+        Toggle[] featureToggles = new Toggle[len];
+        float parentHeight = transform.GetComponent<RectTransform>().sizeDelta.y;
+        float distBetween = -1 * (parentHeight / len);
+
+        // Adding image features
+        for (int i = 1; i < len - 1; i++)   
+        {
+            featureObjs[i] = (GameObject) Instantiate(Resources.Load("ImageFeature"));
+            featureObjs[i].transform.SetParent(transform, false);
+            featureObjs[i].transform.localScale = new Vector3(1, 1, 1);
+            featureObjs[i].transform.localPosition = featureObjs[i].transform.localPosition + new Vector3(0, -56 + distBetween * (i) * 0.9f, 0);
+            featureToggles[i] = featureObjs[i].GetComponent<Toggle>();
+            featureToggles[i].GetComponentInChildren<Text>().text = featureNames[i].Content;
+        }
+
+    }
+
+}
